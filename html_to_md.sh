b247582698b3e5e20b03b3fd7f2bd1302b83ca22
Anton Cherevaty <Cherevaty@avp.ru>
2020-01-09 11:29:21 +0300

    update files

--- html_to_md.sh
+++ html_to_md.sh
@@ -2,7 +2,7 @@
 
 # sed 's/^.*\(title icon-title.*wp_rp_wrap\).*$/\1/' $1 > test.html
 
-cat $1 | sed '1,/icon-title/d' | sed -n '/wp_rp_wrap/q;p' | sed 's|<p>||g' | sed 's|</p>||g' | sed 's|crayon-plain-wrap|CODESTART|g' | sed 's|</textarea></div>|\nCODEEND|g' | sed  '/CODESTART/c\```' | sed  '/CODEEND/c\```' | sed 's/<[^>]*>//g' | sed '/^$/d' |sed '1s/.*/# & \ /' | sed '2s/.*/***&***/' > test.html
+cat $1 | sed '1,/icon-title/d' | sed -n '/wp_rp_wrap/q;p' | sed 's|<p>||g' | sed 's|</p>||g' | sed 's|crayon-plain-wrap|CODESTART|g' | sed 's|</textarea></div>|\nCODEEND|g' | sed  '/CODESTART/c\```' | sed  '/CODEEND/c\```' | sed 's/<[^>]*>//g' | sed '/^$/d' |sed '1s/.*/# & \ /' | sed '2s/.*/***&***\n/' | sed  '2s|  ||' | sed  '2s|  ||' > test.html
 
-cat $1 | sed '1,/icon-title/d' | sed -n '/wp_rp_wrap/q;p' | sed 's|<p>||g' | sed 's|</p>||g' | sed 's|crayon-plain-wrap|CODESTART|g' | sed 's|</textarea></div>|\nCODEEND|g' | sed  '/CODESTART/c\```' | sed  '/CODEEND/c\```' | sed 's/<[^>]*>//g' | sed '/^$/d' | sed '1s/.*/# & \ /' | sed '2s/.*/***&***/' > test.md
+cat $1 | sed '1,/icon-title/d' | sed -n '/wp_rp_wrap/q;p' | sed 's|<p>||g' | sed 's|</p>||g' | sed 's|crayon-plain-wrap|CODESTART|g' | sed 's|</textarea></div>|\nCODEEND|g' | sed  '/CODESTART/c\```' | sed  '/CODEEND/c\```' | sed 's/<[^>]*>//g' | sed '/^$/d' | sed '1s/.*/# & \ /' | sed '2s/.*/***&***\n/' | sed  '2s|  ||' | sed  '2s|  ||' > test.md
 
